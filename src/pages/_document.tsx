c8d085f0820a3a8257d65e7a3f79267950e9e6e4
anderson <andersonvieira818@yahoo.com.br>
2022-08-03 08:42:16 -0300

    feat: global styles and starting components creation

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript
+} from '../../node_modules/next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { Header } from '../components/Header/index'
+
+export default function Home() {
+  return <Header />
+}
